0ec66093071197b8d4a6af77d7211f466a08c623
batuhanysengul <batuhanyigitsengul@gmail.com>
2024-01-05 15:36:25 +0300

    column width'e müdahale edilemiyor?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import './App.css';
+
+const GridColumns = () => (
+  <>
+    <Column field="userId" title="UID" className="column-uid" />
+    <Column field="id" title="ID" className="column-id" />
+    <Column field="title" title="Title" className="column-title" />
+    <Column field="body" title="Body" className="column-body" />
+  </>
+);
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(data => setProducts(data));
+      .then(data => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setLoading(false);
+      });
   }, []);
 
   return (
-    products.length > 0 ? (
-      <Grid style={{ height: '100vh' }} data={products}>
-        <Column field="id" title="ID" width="50px" />
-        <Column field="title" title="Title" width="400px" />
-        <Column field="body" title="Body" width="400px" />
-      </Grid>
-    ) : (
+    loading ? (
       <div>Loading...</div>
+    ) : (
+      <Grid style={{ height: '100vh' }} data={products}>
+        <GridColumns />
+      </Grid>
     )
   );
 };
