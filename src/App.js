1b202630a30dac9f5b5dd8d2c7c7edb26ae1d29e
batuhanysengul <batuhanyigitsengul@gmail.com>
2024-01-05 14:53:27 +0300

    dummy data changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
-import "@progress/kendo-theme-default/dist/all.css";
-import axios from "axios";
+import React from 'react';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
 const App = () => {
-  const [testdata, setTestdata] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        setTestdata(response.data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const products = [
+    { 'ProductID': 1, 'ProductName': 'Chai', 'UnitPrice': 18.0000, 'Discontinued': true },
+    { 'ProductID': 2, 'ProductName': 'Chang', 'UnitPrice': 19.0000, 'Discontinued': false },
+    { 'ProductID': 3, 'ProductName': 'Aniseed Syrup', 'UnitPrice': 10.0000, 'Discontinued': false },
+  ];
 
   return (
-  <Grid 
-    data={testdata}
-  > 
-  </Grid>
-  )
+    <Grid data={products}>
+      <Column field="ProductID" title="ID" width="50px" />
+      <Column field="ProductName" title="Name" width="250px" />
+      <Column field="UnitPrice" title="Price" format="{0:c}" width="80px" />
+      <Column field="Discontinued" title="Discontinued" width="120px" />
+    </Grid>
+  );
 };
 
 export default App;
