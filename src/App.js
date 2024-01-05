7193d5db7853f65932b0a5263ad5dab834d586c1
batuhanysengul <batuhanyigitsengul@gmail.com>
2024-01-05 16:18:06 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
-import './App.css';
+import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
+import "@progress/kendo-theme-default/dist/all.css";
+import "./App.css";
 
 const GridColumns = () => (
   <>
@@ -14,28 +15,49 @@ const GridColumns = () => (
 const App = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10); 
+  const [sort, setSort] = useState([]); 
+
+  const pageChange = (event) => {
+    setPage(event.page);
+    setPageSize(event.pageSize);
+  };
+
+  const sortChange = (event) => {
+    setSort(event.sort);
+  };
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
         setProducts(data);
         setLoading(false);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
         setLoading(false);
       });
   }, []);
 
-  return (
-    loading ? (
-      <div>Loading...</div>
-    ) : (
-      <Grid style={{ height: '100vh' }} data={products}>
-        <GridColumns />
-      </Grid>
-    )
+  return loading ? (
+    <div className="loading">Loading...</div>
+  ) : (
+    <Grid
+      style={{ height: "100vh" }}
+      data={products}
+      pageable={true}
+      onPageChange={pageChange}
+      skip={(page - 1) * pageSize}
+      take={pageSize}
+      total={products.length}
+      sortable={true}
+      sort={sort}
+      onSortChange={sortChange}
+    >
+      <GridColumns />
+    </Grid>
   );
 };
 
