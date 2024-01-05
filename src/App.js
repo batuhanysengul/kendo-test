6603511672b16932154d9a5134e1546adde4d042
batuhanysengul <batuhanyigitsengul@gmail.com>
2024-01-05 15:15:47 +0300

    jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
 const App = () => {
-  const products = [
-    { 'ProductID': 1, 'ProductName': 'Chai', 'UnitPrice': 18.0000, 'Discontinued': true },
-    { 'ProductID': 2, 'ProductName': 'Chang', 'UnitPrice': 19.0000, 'Discontinued': false },
-    { 'ProductID': 3, 'ProductName': 'Aniseed Syrup', 'UnitPrice': 10.0000, 'Discontinued': false },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
 
   return (
-    <Grid data={products}>
-      <Column field="ProductID" title="ID" width="50px" />
-      <Column field="ProductName" title="Name" width="250px" />
-      <Column field="UnitPrice" title="Price" format="{0:c}" width="80px" />
-      <Column field="Discontinued" title="Discontinued" width="120px" />
-    </Grid>
+    products.length > 0 ? (
+      <Grid style={{ height: '100vh' }} data={products}>
+        <Column field="id" title="ID" width="50px" />
+        <Column field="title" title="Title" width="400px" />
+        <Column field="body" title="Body" width="400px" />
+      </Grid>
+    ) : (
+      <div>Loading...</div>
+    )
   );
 };
 
